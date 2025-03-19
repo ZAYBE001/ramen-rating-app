03d81e7350dc6d5aff470c48eddea65ad030411f
ZAYBE001 <zaybe.maliq@student.moringaschool.com>
2025-03-19 08:30:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const ramens = [
     }
     
     function addSubmitListener() {
-        document.addEventListener('DOMContentLoaded', () => {
+        
             document.querySelector('form').addEventListener('submit', e => {
                 e.preventDefault();
     
@@ -53,7 +53,7 @@ const ramens = [
     
                 e.target.reset(); 
             });
-        });
+        
     }
     function displayRamens() {
         ramens.forEach(ramen => {
